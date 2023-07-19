f01f04d28d4a8c8bd0aecfd64534d08965cd535d
ikikjun <ikikjun@naver.com>
2023-07-19 17:33:59 +0900

    2023-07-19
    Fetch 파일업로드

--- ex011/src/main/java/com/ikik/controller/FileuploadController.java
+++ ex011/src/main/java/com/ikik/controller/FileuploadController.java
@@ -26,7 +26,7 @@ import net.coobird.thumbnailator.Thumbnails;
 
 @Controller
 @Log4j
-public class FileuploadController {
+public class FileuploadController extends CommonRestController{
 
 	@GetMapping("/file/fileupload")
 	public void fileupload() {
@@ -52,102 +52,37 @@ public class FileuploadController {
 	public String fileAction(List<MultipartFile> files
 								, int bno
 								, RedirectAttributes rttr) {
-		int insertRes = 0;
+		
+		int insertRes = fileupload(files, bno);
 		
 		// 람다식는 외부에서 사용하는 전역변수를 사용할수 없다고 에러가 납니다.
 		// Local variable insertRes defined in an enclosing scope must be final or effectively final
 		// continue 나 break 는 forEach에서 사용할수없다.
 //		files.forEach(file ->{
-		// 향상된 for문
-		for(MultipartFile file : files) {
-			// 선택된 파일이 없는 경우 다음 파일로 이동
-			if(file.isEmpty()) {
-				continue;
-			}
-			// 파일이 비었을때는 그냥 넘어가도록 처리
-//			if(!file.isEmpty()) {
-			log.info("=== ofileName : " + file.getOriginalFilename());
-			// jsp 에서 넘긴이름
-			log.info("=== Name : " + file.getName());
-			log.info("=== Size : " + file.getSize());
-			
-			try {
-				// UUID
-				/**
-				 * universally unique identifier
-				 * 소프트웨어 구축에 쓰이는 식별자(중복되지않는 값) 표준
-				 * 파일이름이 중복되어 파일이 소실되지 않도록 uuid를 붙여서 저장 
-				 */
-				UUID uuid = UUID.randomUUID();
-				String saveFileName = uuid + "_" + file.getOriginalFilename();
-				// 경로지정
-//				File sFile = new File(pathname);
-				
-				String uploadPath = getFolder();
-				// dir 경로
-				// c:/upload/2023/7/18/  ▶ return str.replace("-", File.separator);
-				// 년/월/일 (폴더안에) 추가
-				File sFile = new File(ATTACHS_DIR 
-											// 경로 (2023\07\18) 반환
-											+ uploadPath
-											+ saveFileName);
-				
-				// file(원본파일) sFile(저장 대상 파일)에 저장
-				file.transferTo(sFile);
-				
-				FileuploadVO vo = new FileuploadVO();
-				// 주어진 파일의 Mime유형
-				String contentType = Files.probeContentType(sFile.toPath());
-				
-				// Mime타입을 확인하여 이미지인 경우 썸네일을 생성
-				if(contentType != null
-						&& contentType.startsWith("image")) {
-					vo.setFileType("I");
-					String thmbnail = ATTACHS_DIR 
-							// 경로 (2023\07\18) 반환
-							+ uploadPath
-							+ "s_"
-							+ saveFileName;
-					// 썸네일 생성
-					// 원본파일, 크기, 저장될경로
-//					Thumbnails.of(원본파일).size(크기).toFile(파일경로);
-					Thumbnails.of(sFile).size(100,100).toFile(thmbnail);
-				} else {
-					vo.setFileType("F");
-				}
-				
-				vo.setBno(bno);
-				vo.setFileName(file.getOriginalFilename());
-//				vo.setFileType("I");
-				vo.setUploadPath(uploadPath);
-				vo.setUuid(uuid.toString());
-				
-				// 서버에 저장하기
-				int res = service.insert(vo);
-				
-				if(res>0) {
-					insertRes++;
-				}
-				
-				
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		// 향상된 for문 공통로직으로 처리 - 호출만 할꺼임 public int fileupload() {
+
 //		}	
 //		});
 		// return "file/fileupload";
 		// 루트 경로 잡아주셔야 합니다 안잡아주시면 현재경로에서 두번 나오게 됩니다!
 		// url은 처음 받아오는 요청에 의해서 받아오는거거든요!
-		
 		String msg = insertRes + "건 저장 되었습니다.";
 		rttr.addAttribute("msg", msg);
 		return "redirect:/file/fileupload";
 //		return "redirect:/file/fileupload?msg="+msg;
+		// ★페이지를 요청합니다 응답으로 jsp 파일을 보내줍니다.★
+	}
+	
+	@PostMapping("/file/fileuploadActionFetch")
+//	public void fileAction(List<MultipartFile> files) {
+	public @ResponseBody Map<String, Object> fileActionFetch(List<MultipartFile> files
+								, int bno) {
+		
+		log.info("fileuploadActionFetch");
+		int insertRes = fileupload(files, bno);
+		log.info("업로드 건수 : " + insertRes);
+		return responseMapMessage("success", insertRes + "건 저장 되었습니다.");
+
 	}
 	
 	
@@ -200,5 +135,101 @@ public class FileuploadController {
 		
 		System.out.println("======CurrentDate : " + currentDate);
 		System.out.println("======경로 : " + uploadPath);
+	}
+	
+	/**
+	 * 첨부파일 저장 및 데이터 베이스에 등록
+	 * @param files
+	 * @param bno
+	 * @return
+	 */
+	public int fileupload(List<MultipartFile> files
+							, int bno)  {
+		
+		int insertRes = 0;
+		
+		for(MultipartFile file : files) {
+			// 선택된 파일이 없는 경우 다음 파일로 이동
+			if(file.isEmpty()) {
+				continue;
+			}
+			// 파일이 비었을때는 그냥 넘어가도록 처리
+//			if(!file.isEmpty()) {
+			log.info("=== ofileName : " + file.getOriginalFilename());
+			// jsp 에서 넘긴이름
+			log.info("=== Name : " + file.getName());
+			log.info("=== Size : " + file.getSize());
+			
+			try {
+				// UUID
+				/**
+				 * universally unique identifier
+				 * 소프트웨어 구축에 쓰이는 식별자(중복되지않는 값) 표준
+				 * 파일이름이 중복되어 파일이 소실되지 않도록 uuid를 붙여서 저장 
+				 */
+				UUID uuid = UUID.randomUUID();
+				String saveFileName = uuid + "_" + file.getOriginalFilename();
+				// 경로지정
+//				File sFile = new File(pathname);
+				
+				String uploadPath = getFolder();
+				// dir 경로
+				// c:/upload/2023/7/18/  ▶ return str.replace("-", File.separator);
+				// 년/월/일 (폴더안에) 추가
+				File sFile = new File(ATTACHS_DIR 
+											// 경로 (2023\\07\\18) 반환
+											+ uploadPath
+											+ saveFileName);
+				
+				// file(원본파일) sFile(저장 대상 파일)에 저장
+				file.transferTo(sFile);
+				
+				FileuploadVO vo = new FileuploadVO();
+				// 주어진 파일의 Mime유형
+				String contentType = Files.probeContentType(sFile.toPath());
+				
+				// Mime타입을 확인하여 이미지인 경우 썸네일을 생성
+				if(contentType != null
+						&& contentType.startsWith("image")) {
+					vo.setFileType("I");
+					String thmbnail = ATTACHS_DIR 
+							// 경로 (2023\07\18) 반환
+							+ uploadPath
+							+ "s_"
+							+ saveFileName;
+					// 썸네일 생성
+					// 원본파일, 크기, 저장될경로
+//					Thumbnails.of(원본파일).size(크기).toFile(파일경로);
+					Thumbnails
+							.of(sFile)
+							.size(100,100)
+							.toFile(thmbnail);
+				} else {
+					vo.setFileType("F");
+				}
+				
+				vo.setBno(bno);
+				vo.setFileName(file.getOriginalFilename());
+//				vo.setFileType("I");
+				vo.setUploadPath(uploadPath);
+				vo.setUuid(uuid.toString());
+				
+				// 서버에 저장하기
+				int res = service.insert(vo);
+				
+				if(res>0) {
+					insertRes++;
+				}
+				
+				
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return insertRes;
 	}
 }
--- ex011/src/main/webapp/WEB-INF/views/file/fileupload.jsp
+++ ex011/src/main/webapp/WEB-INF/views/file/fileupload.jsp
@@ -8,10 +8,91 @@
 <script type="text/javascript">
 	
 	window.addEventListener('load', function(){
+		
+		// 리스트 조회
 		btnList.addEventListener('click', function(){
 			getFileList();
 		})
-	});
+		
+		// 파일 업로드
+		btnFileupload.addEventListener('click', function(){
+			// 웹개발에서 HTML 폼 데이터를
+			// JavaScript로 쉽게 조작하고 전송하는 방법을 제공하는 API 입니다
+			let formData = new FormData(fileuploadForm);
+			formData.append('name', 'ikik');
+			
+			console.log("=====formData : ", formData);
+			
+			// FormData값 확인
+			// 이름, 값 배열에 들어있다 폼에있는 요소가 수집되는것 inputType이 file 이면 objectType으로 들어옵니다.
+			for(var pair of formData.entries()){
+				console.log(pair);
+				console.log(pair[0] + ' : ' + pair[1]);
+//				console.log(pair[0] + ' : ' + pair[1] + ' : ' + pair[2]); // 궁금해서 찍어봄 undefiend 나옴
+				if(typeof(pair[1]) == 'object'){
+//					let fileName = [pair[1].name];
+//					let fileSize = [pair[1].size];
+					let fileName = pair[1].name;
+					let fileSize = pair[1].fileSize;
+					// 파일 확장자, 크기 체크
+					// 서버에 전송 가능한 형식인지 확인
+					// 최대 전송가능한 용량을 초과 하지 않는지
+					
+//					console.log("fileName : ",[pair[1].name]);
+//					console.log("fileSize : ",[pair[1].size]);
+					
+					if(!checkExtension(fileName, fileSize)){
+						return false;	
+					}
+					
+					
+					console.log("fileName : ", fileName);
+					console.log("fileSize : ", fileSize);
+				}
+			}
+//			fetch('/file/fileuploadAction'
+			fetch('/file/fileuploadActionFetch'
+					,{
+						method : 'post'
+						, body : formData
+					})
+				.then(response => response.json()) // 함수라 실행해야합니다. .json()
+				.then(map => fileuploadRes(map))
+//				.then(map => console.log(map))
+//				.then(map => (map) =>{
+//					console.log('map : ' + map);
+//				});
+		});
+		
+	})
+	
+	function checkExtension(fileName, fileSize){
+			let maxSize = 1024 * 1024 * 10;
+			// $는 ~로 끝나는
+			// .exe, .sh, .zip, .alz 끝나는 문자열
+			// 정규표현식 : 특정 규칙을 가진 문자열을 검색하거나 치환 할때 사용
+			let regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+			if(maxSize <= fileSize){
+				alert("파일 사이즈 초과");
+				return false;
+			}
+			
+			// 문자열에 정규식 패턴을 만족하는 값이 있으면 true, 없으면 flase를 리턴한다
+			// TEST 할때 false 값을 주어 아니면.. if(!regex.test(fileName)){
+			if(regex.test(fileName)){
+				alert("해당 종류의 파일은 업로드 할 수 없습니다.")
+				return false;
+			}
+			return true;
+		}
+	
+	// 콜백함수 대신 ?? fileuploadRes 함수 만들어서 콜백
+	function fileuploadRes(map){
+			if(map.result=='success'){
+				divFileuploadRes.innerHTML = map.msg;
+			}
+		}
+		
 
 	function getFileList(){
 		// /file/list/{bno}
@@ -27,7 +108,8 @@
 		let content = '';
 		if(map.list.length > 0){
 			map.list.forEach(function(item, index){
-				content += item.fileName + "/" + item.savePath + '<br>';
+//				content += item.fileName + "/" + item.savePath + '<br>';
+				content += item.fileName + '<br>';
 			})
 		} else {
 			content = '등록된 파일이 없습니다.';
@@ -48,19 +130,23 @@
 		--%>
 		bno : <input type="text" id="bno" name ="bno" value="30">
 		<br>
-		<input type="file" name="files"> <br>
+		<input type="file" name="files" multiple="multiple"> <br>
 		<input type="file" name="files"> <br>
 		<input type="file" name="files"> <br>
 		
 		<button type="submit">파일업로드</button>
+		<button type="button" id="btnFileupload">Fetch 파일업로드</button>
 		<br>
+		<div id="divFileuploadRes">
 		res : ${param.msg }
-		
+		</div>
 	</form>
 	
 	<h2> 파일 리스트 조회 </h2>
 	<button type="button" id="btnList">리스트 조회</button>
-	<div id="divFileupload"></div>
+	<div id="divFileupload">
+	
+	</div>
 	
 </body>
 </html>
