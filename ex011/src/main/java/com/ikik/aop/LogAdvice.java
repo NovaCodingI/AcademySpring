e729dc2a93ef2e0b98534c56c9ddf1b33e0b3db1
ikikjun <ikikjun@naver.com>
2023-07-27 11:32:56 +0900

    20230727

--- ex011/src/main/java/com/ikik/aop/LogAdvice.java
+++ ex011/src/main/java/com/ikik/aop/LogAdvice.java
@@ -3,14 +3,11 @@ package com.ikik.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StopWatch;
 
 import com.ikik.service.LogService;
 import com.ikik.vo.LogVO;
